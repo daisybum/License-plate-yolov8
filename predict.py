be74cd859bd13e83971d7ffecc41d69a0467d1f3
Sanghyun Park <shpark@mv-w.com>
2024-02-17 17:12:18 +0900

    ìëUpdate Unicar project

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO(model='runs\\detect\\train3\\weights\\platev8n.pt')  # build a new model from scratch
+    model = YOLO(model='runs\\detect\\train_epoch20\\weights\\best.pt')  # build a new model from scratch
 
     # Use the model
     results = model.predict('data\\demo.mp4', save=True, imgsz=640, conf=0.01)
--- train.py
+++ train.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    model = YOLO("yolov8x.yaml")  # build a new model from scratch
 
     # Use the model
-    results = model.train(data="configs.yaml", epochs=20, verbose=True, device=0)  # train the model
+    results = model.train(data="configs\\config.yaml", epochs=20, verbose=True, device=0)  # train the model
     # yolo task=detect mode=train data="configs.yaml" epochs=20 verbose=True
